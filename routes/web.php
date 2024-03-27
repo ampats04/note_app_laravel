57c707670dedcd4397d10734f331dbfb1987b69c
ampats04 <jeremyandyampatin@gmail.com>
2024-03-27 17:44:54 +0800

    feature(api): modified registration and login api

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,20 @@
 <?php
 
-use App\Http\Controllers\makeController;
-use App\Http\Controllers\NoteController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [makeController::class, 'welcome'])->name('welcome');
+Route::get('/', function () {
+    return view('welcome');
+});
 
-// Route::get('/note', [NoteController::class, 'index'])->name('index');
-// Route::get('/note/create', [NoteController::class, 'create'])->name('create');
-// Route::post('/note/store', [NoteController::class, 'store'])->name('store');
-// Route::get('/note/{id}/show', [NoteController::class, 'show'])->name('show');
-// Route::get('/note/{id}/edit', [NoteController::class, 'edit'])->name('edit');
-// Route::put('/note/{id}/update', [NoteController::class, 'update'])->name('update');
-// Route::delete('/note/{id}/delete', [NoteController::class, 'delete'])->name('delete');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::resource('note', NoteController::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
