08b8bd970baeb0b4705b6ccf20987a19cfc1e437
ampats04 <jeremyandyampatin@gmail.com>
2024-03-27 15:13:30 +0800

    routes(api): added routes for the notes model

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,15 @@
 <?php
 
 use App\Http\Controllers\makeController;
+use App\Http\Controllers\NoteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [makeController::class, 'welcome'])->name('welcome');
+
+Route::get('/note', [NoteController::class, 'index'])->name('index');
+Route::get('/note/create', [NoteController::class, 'create'])->name('create');
+Route::post('/note', [NoteController::class, 'store'])->name('store');
+Route::get('/note/{id}', [NoteController::class, 'show'])->name('show');
+Route::get('/note/{id}/edit', [NoteController::class, 'edit'])->name('edit');
+Route::put('/note/{id}', [NoteController::class, 'update'])->name('update');
+Route::delete('/note/{id}', [NoteController::class, 'delete'])->name('delete');
